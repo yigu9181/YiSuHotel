441f91a08a43ee080d4bc79d42828f88ec2c41ca
yigu9181 <yigu9181@qq.con>
2026-02-11 23:09:44 +0800

    feat: 完成首页时间和地点的模版

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -3,6 +3,7 @@ import { useLoad } from '@tarojs/taro'
 import { useState } from 'react'
 import classNames from 'classnames'
 import './index.scss'
+
 export default function Index() {
   useLoad(() => {
     console.log('酒店查询页加载')
@@ -19,12 +20,13 @@ export default function Index() {
   }
   return (
     <View className='index'>
-      <View className='advertisement'>广告</View>
-      <view className='message'>
+      <View className='advertisement'></View>
+      <View className='message'>
+        <View className='pos-time'>
         <View className='position'>
-          <Picker 
-            mode='selector' 
-            range={array} 
+          <Picker
+            mode='selector'
+            range={array}
             onChange={onPickerChange}
             onClick={rotate}
             onCancel={rotate}
@@ -34,17 +36,33 @@ export default function Index() {
             </View>
             <Text className={classNames('iconfont', 'icon-jiantou-copy','arrow-up', {
             'arrow-down': isOpen
-            })} />
+            })}
+            />
           </Picker>
-          <View className="input">
-            <Input placeholder="位置/品牌/酒店"></Input>
+          <View className='input'>
+            <Input placeholder='位置/品牌/酒店'></Input>
           </View>
           <View className='iconfont icon-dingwei icon'></View>
         </View>
+        <View className='date'>
+          <View className='check-in'>
+            <Text className='check-in-date'>2月13日</Text>
+            <Text className='check-in-week'>周五</Text>
+          </View>
+          <View className='decoration'></View>
+          <View className='check-out'>
+            <Text className='check-out-date'>2月14日</Text>
+            <Text className='check-out-week'>周六</Text>
+          </View>
+          <View className='time-interval'>共1晚</View>
+        </View>
+        </View>
+        <View className='fil-lab'>
         <Button className='search'>
           <Text>查询</Text>
         </Button>
-      </view>
+        </View>
+      </View>
     </View>
   )
 }
