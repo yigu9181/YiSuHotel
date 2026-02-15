edcbeb27649a0d797efbf72a31fdf630d29422c9
yigu9181 <yigu9181@qq.con>
2026-02-15 16:01:49 +0800

    feat: 完成列表页中酒店展示内容的编写

--- src/pages/list/index.tsx
+++ src/pages/list/index.tsx
@@ -1,8 +1,10 @@
-import { View, Text, Input } from '@tarojs/components'
+import { View, Text, Input, Image } from '@tarojs/components'
 import Taro, { useLoad } from '@tarojs/taro'
 import { setSelectedTheAddress } from '@/store/address/positionAddress'
 import { useSelector , useDispatch } from 'react-redux'
 import { useState } from 'react'
+import hotelImage from '../../asset/pictures/酒店1.png';
+import tagImage from '../../asset/pictures/钻石_填充.png';
 import './index.scss'
 
 
@@ -65,8 +67,8 @@ export default function Index () {
     <View className='index'>
       <View className='tab'>
         <View className='iconfont icon-jiantou-copy icon' onClick={()=>Taro.navigateBack({
-                delta: 1
-              })}
+            delta: 1
+          })}
         >
         </View>
         <View className='tab-form'>
@@ -106,8 +108,8 @@ export default function Index () {
           ))}
         </View>
       </View>
-      <View className='Label'>
-        {arrows.map((arrow, index) => (
+      {arrows.length > 0 && <View className='Label'>
+       {arrows.map((arrow, index) => (
           <View
             key={arrow.type}
             className={`arrow ${index === 0 ? '' : 'arrow2'}`}
@@ -115,6 +117,48 @@ export default function Index () {
             {index !== 0 && <View className='tail'></View>}
             <View className='body'>{arrow.content}</View>
             <View className='head'></View>
+          </View>
+        ))}
+      </View>
+      }
+      <View className='hotel'>
+
+        <View className='shadow-sticky'>
+          {/*用于添加阴影*/}
+        </View>
+        {/*酒店展示组件*/}
+        {[0, 1, 2,3,4].map((i) => (
+          <View className='hotel-item' key={i}>
+            <Image className='hotel-picture' src={hotelImage} />
+            <View className='hotel-text'>
+              <View className='hotel-name'>
+                <Text className='hotel-name-text'>上海陆家嘴禧玥酒店</Text>
+                <View className='hotel-tag-box'>
+                  {/*用于添加钻石*/}
+                  {[0, 1, 2, 3, 4].map((j) => (
+
+                    <Image className='hotel-tag' key={j} src={tagImage} />
+                  ))}
+                </View>
+              </View>
+              <View className='hotel-evaluation'>
+                <View className='hotel-point'>4.8</View>
+                <View className='hotel-rank'>超棒</View>
+                <View className='hotel-like'>4564点评 · 6.3万收藏</View>
+              </View>
+              <View className='hotel-position'>
+                近外滩 · 东方明珠
+              </View>
+              <View className='hotel-introduction'>BOSS:25楼是上海知名米其林新荣记</View>
+              <View className='hotel-label'>
+              {['免费升房', '新中式风', '一线江景','免费停车'].map((label) => (
+                <View className='hotel-label-item' key={label}>{label}</View>
+              ))}
+              </View>
+              <View className='hotel-Ranking'>上海美景酒店榜 No.16</View>
+              <View className='hotel-price'><Text style='font-size: 15px'>￥</Text>936<Text style='font-size: 12px;margin-left:2px'>起</Text></View>
+              <View className='hotel-supplement'>{'钻石贵宾价 满减券 3项优惠98'}<Text className='icon-jiantou-copy iconfont icon-rotate'></Text></View>
+            </View>
           </View>
         ))}
       </View>
